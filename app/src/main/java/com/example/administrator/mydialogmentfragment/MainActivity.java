9e9b9d2ddcc70b150e74a4b4a1ed9cedf81d357a
dijiubuzhan <1012987012@qq.com>
2021-01-23 11:24:58 +0800

    添加主界面Log

--- app/src/main/java/com/example/administrator/mydialogmentfragment/MainActivity.java
+++ app/src/main/java/com/example/administrator/mydialogmentfragment/MainActivity.java
@@ -4,15 +4,18 @@ import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MyLog";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: ");
     }
 
     public void show(View view) {
